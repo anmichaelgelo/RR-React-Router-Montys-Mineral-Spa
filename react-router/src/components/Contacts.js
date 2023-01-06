e3589a5b5e18d864b5911c2c1c690eef83059f75
Michael Agoncillo <108847743+anmichaelgelo@users.noreply.github.com>
2023-01-06 10:33:42 +0000

    fixed routing

---
+++ react-router/src/components/Contacts.js
@@ -0,0 +1,13 @@
+export default function Contacts () {
+    return (
+        <div className="contactsPage">
+            <br />
+            <br />
+            <div className="contactsHeader">
+                <h1>Contact Us</h1>
+            </div>
+            <p>example@gmail.com</p>
+            <p>+987654312</p>
+        </div>
+    )
+}
